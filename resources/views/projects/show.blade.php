a0216ce064db756cba5d1dca96f64f2225d1c7e5
Sammy Mwangangi <sammymwinzi2@gmail.com>
2021-06-08 14:59:33 +0300

    updates

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,11 +6,11 @@
                 <h1 class="font-bold text-xl text-gray-800 leading-tight">Back to Projects Board</h1>
             </a>
             <div>
-                <a href="{{route('projects.create')}}" class="flex items-center px-4 py-1 text-xs text-red-600 font-semibold rounded-full border border-red-300 hover:text-white hover:bg-red-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">
+                <a href="{{route('tasks.create')}}" class="flex items-center px-4 py-1 text-xs text-red-600 font-semibold rounded-full border border-red-300 hover:text-white hover:bg-red-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                     </svg>
-                    New Project
+                    Add Task
                 </a>
 
             </div>
