165238b9d95fb23e3443c336e0b2e2b657a71d93
Sammy Mwangangi <sammymwinzi2@gmail.com>
2021-06-23 15:04:44 +0300

    changes

--- resources/views/livewire/task-navigation.blade.php
+++ resources/views/livewire/task-navigation.blade.php
@@ -52,10 +52,10 @@
                             <div class="relative w-8 h-8 items-center">
                                 <svg class="rounded-full border border-gray-100 shadow-sm" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
 
-                                @if(auth()->user()->notifications->count())
+                                {{-- @if(auth()->user()->notifications->count())
                                     <div class="absolute top-0 right-0 h-3 w-3 text-xs text-white ml-4 my-1 border-2 border-white rounded-full bg-gray-200 z-2 text-center">
                                     </div>
-                                @endif
+                                @endif --}}
                             </div>
                         </x-slot>
                         <x-slot name="content">
