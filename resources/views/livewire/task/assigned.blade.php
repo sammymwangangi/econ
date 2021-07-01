905962dab9f6a5fcf8a2d030b483120b1aa37e65
Sammy Mwangangi <sammymwinzi2@gmail.com>
2021-07-01 12:31:27 +0300

    createdBy and assignedTo updates

--- resources/views/livewire/task/assigned.blade.php
+++ resources/views/livewire/task/assigned.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-wrap items-center gap-2 px-4 py-2">
     @foreach($tasks as $task)
-    <div class="text-xs bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 px-2 py-1 rounded-full cursor-pointer text-white">{{$task->name}}</div>
+    <a href="{{route('tasks.show', $task->id)}}" class="text-xs bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 px-2 py-1 rounded-full cursor-pointer text-white">{{$task->name}}</a>
     @endforeach
 </div>
